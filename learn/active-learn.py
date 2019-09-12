ad7ba92b8ed43ad059e67d9bff4ef1bbae3e2a58
Chenglong Wang <mestway@gmail.com>
2019-09-12 11:44:46 -0700

    update active learn function

--- learn/active-learn.py
+++ learn/active-learn.py
@@ -11,13 +11,16 @@ import argparse
 parser = argparse.ArgumentParser(description="codar active learning cli")
 
 # data and label files
-parser.add_argument('--data_file', type=str, default="pairs.json", help="the dataset pool")
-parser.add_argument('--labels', type=str, default='{"1": ">"}', help="labels of the dataset: >, <, bad, unlabeled")
+parser.add_argument('--data_file', type=str, default="example_pairs.json", help="the dataset pool")
+parser.add_argument('--labels', type=str, default='{"1": ">", "2": ">", "3": ">", "4": ">", "5": ">", "6": ">", "7": ">", "8": ">", "9": ">", "10": ">", "11": ">", "12": ">", "13": ">", "14": ">", "15": ">", "16": ">"}', 
+                    help="labels of the dataset: >, <, bad, unlabeled")
 
 # active learning parameters
 parser.add_argument('--min_label_size', type=int, default=15, help="the minimum number of labels we need to start active learning")
 parser.add_argument('--sample_size', type=int, default=5, help="the number of samples will be presented for labeling at each time")
 parser.add_argument('--sample_func', type=str, default="entropy", help="sample function, one of [entropy], [margin], [random]")
+parser.add_argument('--prefer_low_complexity_pairs', type=bool, default=True, help="whether prefer low complexity pairs first")
+
 
 def get_feature(chart):
     """Given a vis json object, parse its feature """
@@ -26,7 +29,10 @@ def get_feature(chart):
         body = rule[rule.index("(") + 1: rule.index(")")]
         literals = body.strip().split(",")
         return head, literals
-    facts = [x for x in json.loads(chart["draco"]) if x.startswith("soft")]
+
+    all_facts = json.loads(chart["draco"]) if isinstance(chart["draco"], (str,)) else chart["draco"]
+    facts = [x for x in all_facts if x.startswith("soft")]
+
     feature = {}
     for r in facts:
         h, l = parse_soft_rule(r)
@@ -47,7 +53,7 @@ def get_pair_feature(pair):
         combined[k] -= v
     return combined
 
-def active_learn_one_iter(model, X_label, y_label, X_pool, sample_func):
+def active_learn_one_iter(model, X_label, y_label, X_pool, sample_func, sample_size):
     """one iteration of active learning """
     model.fit(X_label, y_label)
     
@@ -78,14 +84,13 @@ if __name__ == '__main__':
         dataset = json.load(f)
 
     labels = {int(pair_id): label for pair_id, label in json.loads(args.labels).items()}
-    #labels = { e["pair_id"] : e["label"] for e in raw_labels }
 
     sample_func = args.sample_func
     sample_size = args.sample_size
     min_label_size = args.min_label_size
 
-
     features = { p["pair_id"]: get_pair_feature(p) for p in dataset }
+    complexity = { p["pair_id"]: len(json.loads(p["left"]["draco"]) + json.loads(p["right"]["draco"])) for p in dataset }
 
     # get all features used by all tools
     all_features_names = list(set([k for p in features.values() for k in p.keys()]))
@@ -98,7 +103,8 @@ if __name__ == '__main__':
                         if (pair_id in labels and labels[pair_id] in ["<", ">"])]
 
     pool_data = [{"feature": feature,
-                  "pair_id": pair_id} for pair_id, feature in features.items() 
+                  "pair_id": pair_id,
+                  "complexity": complexity[pair_id]} for pair_id, feature in features.items() 
                  if (pair_id not in labels or labels[pair_id] == "unlabeled")]
 
     if len(labeled_data) < min_label_size:
@@ -106,13 +112,39 @@ if __name__ == '__main__':
         selections = np.random.choice(list(range(len(pool_data))), min_label_size - len(labeled_data))
         to_label_ids = [pool_data[k]["pair_id"] for k in selections]
     else:
-        X_label = np.array([[(p["feature"][feature_name] if feature_name in p["feature"] else 0) 
-                            for feature_name in all_features_names] for p in labeled_data])
-        y_label = np.array([p["label"] for p in labeled_data])
+        # the cross-validation size used for calibration
+        calibration_cv_size = 5
+
+        X_label = [[(p["feature"][feature_name] if feature_name in p["feature"] else 0) 
+                            for feature_name in all_features_names] for p in labeled_data]
+        y_label = [p["label"] for p in labeled_data]
+
+        # flip some labels so that we get balanced x, y
+        label_to_flip = "<" if y_label.count(">") < calibration_cv_size else (">" if y_label.count("<") < calibration_cv_size else None)
+        if label_to_flip is not None:
+            flip_count = calibration_cv_size - (len(y_label) - y_label.count(label_to_flip))
+            for i in range(len(X_label)):
+                if y_label[i] == label_to_flip:
+                    X_label[i] = [-x for x in X_label[i]]
+                    y_label[i] = ">" if y_label[i] == "<" else "<"
+                    flip_count -= 1
+                if flip_count <= 0:
+                    break    
+
+        X_label, y_label = np.array(X_label), np.array(y_label)
+
         X_pool = np.array([[(p["feature"][feature_name] if feature_name in p else 0) for feature_name in all_features_names] for p in pool_data])
 
-        model = CalibratedClassifierCV(LinearSVC(fit_intercept=0), cv=5)
-        selections = active_learn_one_iter(model, X_label, y_label, X_pool, sample_func)
+        model = CalibratedClassifierCV(LinearSVC(fit_intercept=0), cv=calibration_cv_size)
+
+        if args.prefer_low_complexity_pairs:
+            selections = active_learn_one_iter(model, X_label, y_label, X_pool, sample_func, sample_size * 2)
+            candidate_complexity = [pool_data[k]["complexity"] for k in selections]
+            lowest_complexity_candidate = np.argsort(candidate_complexity)[:sample_size]
+            # select samples with lower complexity
+            selections = [selections[k] for k in lowest_complexity_candidate]
+        else:
+            selections = active_learn_one_iter(model, X_label, y_label, X_pool, sample_func, sample_size)
 
         to_label_ids = [pool_data[k]["pair_id"] for k in selections]
 
